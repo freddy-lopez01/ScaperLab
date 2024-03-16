a30f20ea8aa00e9bc8889f7c25863ff8133cedef
Freddy Lopez <freddlopez01@gmail.com>
2024-03-16 14:10:26 -0700

    updated some docstrings

--- scaperlab/scripts/imageScrape.py
+++ scaperlab/scripts/imageScrape.py
@@ -1,5 +1,20 @@
 from bs4 import BeautifulSoup 
 import requests
+'''
+webscrope.py will be the backend logic that will webscrape for images found on other websites to pull in and diosplay within ScaperLabs pages
+'''
+
+def main():
+	pass
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
 
 
 
