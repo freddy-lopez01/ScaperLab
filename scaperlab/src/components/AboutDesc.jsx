6b86b8f6bf92c8b8383d5837dbf2c0804fdcc24d
Freddy Lopez <freddlopez01@gmail.com>
2024-03-17 21:52:54 -0700

    worked a tiny bit on the About page cause Im tired of studying

---
+++ scaperlab/src/components/AboutDesc.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './AboutDesc.css';
+
+
+
+function AboutDes () {
+    return (
+        <>
+        <div className="about-body">
+            <img src="pic3.jpg" alt="pic3"/>
+            <h1>About ScaperLab</h1>
+        </div>
+        </>
+    )
+};
+
+export default AboutDes
--- scaperlab/src/components/pages/About.js
+++ scaperlab/src/components/pages/About.js
@@ -1,12 +1,14 @@
 import React from "react";
-
-
+import AboutDes from "../AboutDesc";
+import Footer from "../Footer";
 function About () {
     return (
         <>
         About
+        <AboutDes/>
+        <Footer/>
         </>
     )
-}
+};
 
 export default About
