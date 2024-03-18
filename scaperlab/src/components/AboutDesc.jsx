d251a4b0eedaa87ec3cb7a5324bcec6a7989319c
Freddy Lopez <freddlopez01@gmail.com>
2024-03-17 21:55:03 -0700

    added little tag

--- scaperlab/src/components/AboutDesc.jsx
+++ scaperlab/src/components/AboutDesc.jsx
@@ -9,6 +9,10 @@ function AboutDes () {
         <div className="about-body">
             <img src="pic3.jpg" alt="pic3"/>
             <h1>About ScaperLab</h1>
+            <p>ScaperLab is a repository oif knowlege for all aquascapers regarding nature auqariums.
+                I have worked as a aquascaping specialist for over 3 years and I have helped many people
+                start their journey into the world of Aquascaping.
+            </p>
         </div>
         </>
     )
