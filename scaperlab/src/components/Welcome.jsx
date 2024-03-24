635e2230ac0201f878d792935e5f31eb451fddb6
Freddy Lopez <freddlopez01@gmail.com>
2024-03-24 14:23:50 -0700

    added more to welcome.jsx

--- scaperlab/src/components/Welcome.jsx
+++ scaperlab/src/components/Welcome.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from 'gsap';
+import { useGSAP } from "@gsap/react";
+
+
 import './Welcome.css';
 
 function Welcome () {
+    useGSAP(()=> {
+        let textAnimation = gsap.timeline();
+
+        textAnimation.from('.word', {
+            y: 100,
+            stagger: {
+                each: 0.5
+            }
+        })
+    }, []);
+
     return (
         <>
         <div className="welcome-frame">
-            <h1>Welcome to ScaperLab!</h1>
+            <h1 className="word">Welcome to ScaperLab!</h1>
+        </div>
+
+        <div className="welcome-frameV2">
         </div>
         </>
     )
